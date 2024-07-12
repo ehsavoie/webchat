dd1cbb53d71bf430e88caddfdd02ba23aa341c66
Emmanuel Hugonnet <ehugonne@redhat.com>
2024-07-12 16:01:54 +0200

    Sample using annotations to create AIService
     * Defining tools and checking them with groq
     * Adding Langchain4J AI service
     * Using smallrye-llm 0.0.1
     * Upgrading to WildFly 33
     * Upgrading to Langchain4J 0.35.0
    
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

---
+++ src/main/java/org/wildfly/ai/websocket/Calculator.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.wildfly.ai.websocket;
+
+import dev.langchain4j.agent.tool.Tool;
+
+public class Calculator {
+
+    @Tool("Calculates the length of a string")
+    int stringLength(String s) {
+        System.out.println("Called stringLength() with s='" + s + "'");
+        return s.length();
+    }
+
+    @Tool("Calculates the sum of two numbers")
+    int add(int a, int b) {
+        System.out.println("Called add() with a=" + a + ", b=" + b);
+        return a + b;
+    }
+
+    @Tool("Calculates the square root of a number")
+    double sqrt(int x) {
+        System.out.println("Called sqrt() with x=" + x);
+        return Math.sqrt(x);
+    }
+
+}
---
+++ src/main/java/org/wildfly/ai/websocket/SimpleAIService.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.wildfly.ai.websocket;
+
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import io.smallrye.llm.spi.RegisterAIService;
+import jakarta.enterprise.context.SessionScoped;
+
+@RegisterAIService(chatLanguageModelName = "mychat", tools = {org.wildfly.ai.websocket.Calculator.class}, scope = SessionScoped.class)
+public interface SimpleAIService {
+    @SystemMessage("""
+                   You are an AI named Bob answering general question.
+                   Your response must be polite, use the same language as the question, and be relevant to the question.""")
+    String chat(@UserMessage String question);
+}
