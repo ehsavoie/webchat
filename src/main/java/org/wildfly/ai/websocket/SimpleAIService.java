104bf13991733b0b2f51385cafb6443bb9384f20
Emmanuel Hugonnet <ehugonne@redhat.com>
2024-07-15 17:43:40 +0200

    Adding code with tools
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

--- src/main/java/org/wildfly/ai/websocket/SimpleAIService.java
+++ src/main/java/org/wildfly/ai/websocket/SimpleAIService.java
@@ -1,12 +1,17 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
  */
 package org.wildfly.ai.websocket;
 
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
 import org.wildfly.ai.annotations.RegisterAIService;
 
-@RegisterAIService(chatLanguageModelName = "mychat")
+@RegisterAIService(chatLanguageModelName = "mychat", tools = {org.wildfly.ai.websocket.Calculator.class})
 public interface SimpleAIService {
-    String chat(String question);
+    @SystemMessage("""
+                   You are an AI named Bob answering general question.
+                   Your response must be polite, use the same language as the question, and be relevant to the question.""")
+    String chat(@UserMessage String question);
 }
