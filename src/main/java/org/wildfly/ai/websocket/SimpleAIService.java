b5b65dc5a642f1c28895b292c379f086149ee246
Emmanuel Hugonnet <ehugonne@redhat.com>
2024-11-19 13:52:30 +0100

    Adding a service page and a Weather tool
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

--- src/main/java/org/wildfly/ai/websocket/SimpleAIService.java
+++ src/main/java/org/wildfly/ai/websocket/SimpleAIService.java
@@ -9,9 +9,9 @@ import dev.langchain4j.service.UserMessage;
 import io.smallrye.llm.spi.RegisterAIService;
 import jakarta.enterprise.context.SessionScoped;
 
-
-@RegisterAIService(chatLanguageModelName = "mistral", tools = {org.wildfly.ai.websocket.Weather.class,org.wildfly.ai.websocket.Calculator.class}, scope = SessionScoped.class)
+@RegisterAIService(chatLanguageModelName = "mistral", tools = {org.wildfly.ai.websocket.Weather.class, org.wildfly.ai.websocket.Calculator.class}, scope = SessionScoped.class)
 public interface SimpleAIService {
+
     @SystemMessage("""
                    You are an AI named Bob answering general question.
                    Your response must be polite, use the same language as the question, and be relevant to the question.""")
