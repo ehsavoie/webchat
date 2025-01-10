811a9448886e83f5b2c22bcb7012a7afdec8ebd1
Emmanuel Hugonnet <ehugonne@redhat.com>
2025-01-10 16:38:03 +0100

    WIP
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

---
+++ src/main/java/org/wildfly/ai/websocket/SseBroadcasterStreamingResponseHandler.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2025 JBoss by Red Hat.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.ai.websocket;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.model.StreamingResponseHandler;
+import dev.langchain4j.model.output.Response;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.sse.OutboundSseEvent;
+import jakarta.ws.rs.sse.Sse;
+import jakarta.ws.rs.sse.SseEventSink;
+
+public class SseBroadcasterStreamingResponseHandler implements StreamingResponseHandler<AiMessage> {
+
+    private int lastEventId;
+    private final SseEventSink sseEventSink;
+    private final OutboundSseEvent.Builder eventBuilder;
+    private boolean running = true;
+
+    public SseBroadcasterStreamingResponseHandler(SseEventSink sseEventSink, Sse sse, int lastEventId) {
+        this.lastEventId = lastEventId;
+        this.sseEventSink = sseEventSink;
+        this.eventBuilder = sse.newEventBuilder();
+    }
+
+    @Override
+    public void onNext(String token) {
+        lastEventId++;
+        OutboundSseEvent sseEvent = eventBuilder
+                .name("token")
+                .id(String.valueOf(lastEventId))
+                .mediaType(MediaType.TEXT_PLAIN_TYPE)
+                .data(token)
+                .reconnectDelay(3000)
+                .comment("This is a token from the llm")
+                .build();
+        sseEventSink.send(sseEvent);
+        lastEventId++;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    @Override
+    public void onComplete(Response<AiMessage> response) {
+        synchronized (this) {
+            this.running = false;
+        }
+    }
+
+    @Override
+    public void onError(Throwable error) {
+        error.printStackTrace();
+        synchronized (this) {
+            this.running = false;
+        }
+    }
+}
---
+++ src/main/java/org/wildfly/ai/websocket/SseResource.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2025 JBoss by Red Hat.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.ai.websocket;
+
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.sse.Sse;
+import jakarta.ws.rs.sse.SseEventSink;
+import java.util.List;
+
+@RequestScoped
+@Path("/sse")
+public class SseResource {
+
+    @Inject
+    @Named(value = "streaming-ollama")
+    StreamingChatLanguageModel streamingChatModel;
+
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @Path("/chat")
+    public void streamingChatWithAssistant(@Context Sse sse, @Context SseEventSink sseEventSink,
+            @HeaderParam(HttpHeaders.LAST_EVENT_ID_HEADER) @DefaultValue("-1") int lastReceivedId,
+            @QueryParam("question") String question) {
+        final int lastEventId;
+        if (lastReceivedId != -1) {
+            lastEventId = lastReceivedId + 1;
+        } else {
+            lastEventId = 1;
+        }
+        System.out.println("Receiving a request on SSE endpoint");
+        List<ChatMessage> messages = List.of(SystemMessage.from("""
+                   You are an AI named Bob answering general question.
+                   Your response must be polite, use the same language as the question, and be relevant to the question."""),
+                UserMessage.from(question));
+        SseBroadcasterStreamingResponseHandler handler = new SseBroadcasterStreamingResponseHandler(sseEventSink, sse, lastEventId);
+        while (handler.isRunning()) {
+            streamingChatModel.generate(messages, handler);
+        }
+    }
+}
