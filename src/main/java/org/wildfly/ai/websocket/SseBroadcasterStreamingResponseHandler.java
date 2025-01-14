0fe5fd70beb843c2665774fb5c536a7637115563
James R. Perkins <jperkins@redhat.com>
2025-01-14 15:20:08 -0800

    Update the SSE example to work with any questions.

--- src/main/java/org/wildfly/ai/websocket/SseBroadcasterStreamingResponseHandler.java
+++ src/main/java/org/wildfly/ai/websocket/SseBroadcasterStreamingResponseHandler.java
@@ -15,6 +15,8 @@
  */
 package org.wildfly.ai.websocket;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.model.StreamingResponseHandler;
 import dev.langchain4j.model.output.Response;
@@ -25,23 +27,21 @@ import jakarta.ws.rs.sse.SseEventSink;
 
 public class SseBroadcasterStreamingResponseHandler implements StreamingResponseHandler<AiMessage> {
 
-    private int lastEventId;
+    private final AtomicInteger lastEventId;
     private final SseEventSink sseEventSink;
     private final OutboundSseEvent.Builder eventBuilder;
-    private boolean running = true;
 
     public SseBroadcasterStreamingResponseHandler(SseEventSink sseEventSink, Sse sse, int lastEventId) {
-        this.lastEventId = lastEventId;
+        this.lastEventId = new AtomicInteger(lastEventId);
         this.sseEventSink = sseEventSink;
         this.eventBuilder = sse.newEventBuilder();
     }
 
     @Override
     public void onNext(String token) {
-        lastEventId++;
         OutboundSseEvent sseEvent = eventBuilder
                 .name("token")
-                .id(String.valueOf(lastEventId))
+                .id(String.valueOf(lastEventId.getAndIncrement()))
                 .mediaType(MediaType.TEXT_PLAIN_TYPE)
                 .data(token.replace("\n", "<br/>"))
                 .reconnectDelay(3000)
@@ -50,22 +50,24 @@ public class SseBroadcasterStreamingResponseHandler implements StreamingResponse
         sseEventSink.send(sseEvent);
     }
 
-    public boolean isRunning() {
-        return running;
-    }
-
     @Override
     public void onComplete(Response<AiMessage> response) {
-        synchronized (this) {
-            this.running = false;
-        }
+        OutboundSseEvent sseEvent = eventBuilder
+                .name("token")
+                .id(String.valueOf(lastEventId.getAndIncrement()))
+                .mediaType(MediaType.TEXT_PLAIN_TYPE)
+                .data("end-data-token")
+                .reconnectDelay(3000)
+                .comment("This is a token from the llm")
+                .build();
+        sseEventSink.send(sseEvent)
+                .whenComplete((event, throwable) -> {
+                    sseEventSink.close();
+                });
     }
 
     @Override
     public void onError(Throwable error) {
         error.printStackTrace();
-        synchronized (this) {
-            this.running = false;
-        }
     }
 }
